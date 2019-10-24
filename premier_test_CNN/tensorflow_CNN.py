8e97de28ebb90ee0d1c05a43cda99ed3064ed997
Toyem <36408240+Toyem@users.noreply.github.com>
2019-10-24 14:52:52 +0200

    Dernière modif ?

--- premier_test_CNN/tensorflow_CNN.py
+++ premier_test_CNN/tensorflow_CNN.py
@@ -2,15 +2,32 @@ import tensorflow as tf
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Dropout, Activation, Flatten, Conv2D, MaxPooling2D
 import pickle
+import numpy as np
+
+IMG_SIZE = 50  #50
 
 X_train = pickle.load(open("X_train.pickle", "rb"))
 y_train = pickle.load(open("y_train.pickle", "rb"))
-X_test = pickle.load(open("x_test.pickle", "rb"))
+X_test = pickle.load(open("X_test.pickle", "rb"))
 y_test = pickle.load(open("y_test.pickle", "rb"))
 
+X_test = np.array(X_test).reshape(-1, IMG_SIZE, IMG_SIZE)
+X_train = np.array(X_train).reshape(-1, IMG_SIZE, IMG_SIZE)
+y_test = np.array(y_test)
+y_train = np.array(y_train)
+
+print(y_test)
+'''
+mnist = tf.keras.datasets.mnist
+
+(X_train, y_train), (X_test, y_test) = mnist.load_data()
+
+print(y_test)
+
+'''
 X_train, X_test = X_train / 255.0, X_test / 255.0
 model = tf.keras.models.Sequential([
-  tf.keras.layers.Flatten(input_shape=(28, 28)),
+  tf.keras.layers.Flatten(input_shape=(IMG_SIZE, IMG_SIZE)),
   tf.keras.layers.Dense(128, activation='relu'),
   tf.keras.layers.Dropout(0.2),
   tf.keras.layers.Dense(10, activation='softmax')
