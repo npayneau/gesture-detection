2c2d6ab465542b2c5da212e0cd88b4e1ff245b26
Toyem <36408240+Toyem@users.noreply.github.com>
2019-11-27 11:32:00 +0100

    Java use h5 file

--- Run_Java_WithoutOpenCV/src/main/java/ControleJava/Run.java
+++ Run_Java_WithoutOpenCV/src/main/java/ControleJava/Run.java
@@ -1,5 +1,17 @@
 package ControleJava;
 
+import org.springframework.core.io.ClassPathResource;
+import org.deeplearning4j.nn.multilayer.MultiLayerNetwork;
+import org.deeplearning4j.nn.modelimport.keras.KerasModelImport;
+
+
 public class Run {
+	
+	String simpleMlp = new ClassPathResource("simple_mlp.h5").getFile().getPath();
+	MultiLayerNetwork model = KerasModelImport.importKerasSequentialModelAndWeights(simpleMlp);
+
+	// ...
+
+	INDArray output = model.output(input);
 
 }
