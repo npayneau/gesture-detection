a6303780b2535c1ef24f1933603ce68995e57e91
Kevin Thiebaut <49019427+kThieb@users.noreply.github.com>
2019-11-27 13:52:55 +0100

    Changed capture_image_train

--- Identification/capture_image_train.py
+++ Identification/capture_image_train.py
@@ -15,15 +15,15 @@ import numpy as np
 import time
 #%% Parameters
 
-yA,yB,xA,xB = cim.crop_current_image()
-size = (100,100,3)
-CATEGORIES = ["Poing","Doigt 1","Main Ouverte","2 Doigts","Rien"]
+yA, yB, xA, xB = cim.crop_current_image()
+size = (100, 100, 3)
 lookup = pickle.load(open("lookup.pickle", "rb"))
+CATEGORIES = list(lookup.values())
 reverselookup = pickle.load(open("reverselookup.pickle", "rb"))
 current_path = os.getcwd()
-data_source = os.path.join(current_path,"Dataset")
-data_source = os.path.join(data_source,"Nous")
-data_source = os.path.join(data_source,"Kevin")
+data_source = os.path.join(current_path, "Dataset")
+data_source = os.path.join(data_source, "Nous")
+data_source = os.path.join(data_source, "Kevin")
 
 def capture():
     global xA, xB, yA, yB
@@ -32,7 +32,8 @@ def capture():
     # On Créé un dataset pour chaque geste
     for i in CATEGORIES :
         directory = data_source + '\\' + i
-        os.chdir(directory)
+        if i not in os.listdir(data_source):
+            os.makedirs(directory)
         print("Prepare yourself to do the gesture " + i + "...")
         print("Start is in 5 seconds")
         time.sleep(5)
@@ -40,21 +41,20 @@ def capture():
         current_time = time.time()
         while (current_time - start_time)<20:
             ret, frame = cap.read()
-            if ret == False:
+            if not ret:
                 break
             if cv2.waitKey(1) & 0xFF == ord('q'):
                 break
             img0 = np.copy(frame)
-            cv2.rectangle(img0,(xA,yA),(xB,yB),(0,255,0),1)
-            cv2.imshow('Capturing...',img0)
+            cv2.rectangle(img0, (xA, yA), (xB, yB), (0, 255, 0), 1)
+            cv2.imshow('Capturing...', img0)
             img = np.copy(frame)
-            if xA!=xB and yA!=yB:
-                img=img[min(yA,yB):max(yA,yB),min(xA,xB):max(xA,xB),:]
+            if xA != xB and yA != yB:
+                img = img[min(yA, yB):max(yA, yB), min(xA, xB):max(xA, xB), :]
 #            img = cv2.resize(img,(size[0],size[1]))
-            img = cv2.resize(img,(1000,1000))
-            cv2.imshow("Capturing....",img)
-            cv2.imwrite(filename='super'+str(cnt)+'.jpg',img = img)
-            cnt+=1
+            img = cv2.resize(img, (1000,1000))
+            cv2.imshow("Capturing....", img)
+            cv2.imwrite(os.path.join(directory, str(current_time)+'.jpg'), img=img)
             current_time = time.time()
     # When everything done, release the capture
     cap.release()
