a2d244fbe36b21fc01424927e794bc6cd46c9b57
Jules L <52790189+Seluj53@users.noreply.github.com>
2019-12-18 11:11:55 +0100

    changed model

--- gesture-detection/app/app.py
+++ gesture-detection/app/app.py
@@ -9,7 +9,7 @@ from modules.Video import Video
 # %% Parameters
 camera = 0
 resize = False
-model_name = "model222"
+model_name = "model"
 size = (100, 100, 3)
 
 app = Flask(__name__)
--- gesture-detection/app/train.py
+++ gesture-detection/app/train.py
@@ -8,11 +8,12 @@ from modules.Video import Video
 camera = 0
 resize = False
 model_name = "model"
-dataset_name = "Dataset2"
+dataset_name = "Dataset"
 size = (100, 100, 3)
 maxpardossier = 100
 charge = 1
 batchsize = 32
+epochs = 10
 
 # %% Loading data
 dataset = Dataset(size=size, batchsize=batchsize)
@@ -21,13 +22,14 @@ dataset.load(dataset_name=dataset_name, charge=charge, maxpardossier=maxpardossi
 imagen = ImageDataGenerator(vertical_flip=True)
 # You can also save a tree copy of the dataset in an excel file
 dataset.save_excel()
+
 # %% Training on model
 # Creating a model
 model = CnnModel(model_name=model_name, size=size)
 # Compiling the dataset into an iterator to train
 batchiterator = dataset.compile()
 # Training
-model.train(batchiterator)
+model.train(batchiterator, epochs=epochs)
 # Saving the new model into a copy (if you have written on another model, you can still get it from the backups directory)
 model.save(model_name="final_model")
 
