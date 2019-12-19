12b36e1180b991b50f2f6f01735d992276046ad1
Toyem <36408240+Toyem@users.noreply.github.com>
2019-12-19 09:25:44 +0100

    rechange name file

--- application/application/src/main/java/com/gesturedetection/application/Application.java
+++ application/application/src/main/java/com/gesturedetection/application/Application.java
@@ -1,4 +1,4 @@
-package com.gesturedetection.application.application;
+package com.gesturedetection.application;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
