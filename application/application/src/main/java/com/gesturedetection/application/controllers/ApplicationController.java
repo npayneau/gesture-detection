7c5e72e98432533f084cda573538efde7764269e
Toyem <36408240+Toyem@users.noreply.github.com>
2019-12-19 09:21:51 +0100

    réecriture des commantaires

--- application/application/src/main/java/com/gesturedetection/application/controllers/ApplicationController.java
+++ application/application/src/main/java/com/gesturedetection/application/controllers/ApplicationController.java
@@ -11,18 +11,18 @@ import java.util.List;
 
 import com.gesturedetection.application.services.GesteService;
 
+//*********************************************//
+//        Controleur de l'application          //
+//*********************************************//
 
 @RestController
 public class ApplicationController {
 	
-	private String prev_geste;
+	private String prev_geste;  // Stockage du précédent 
 	
-
-	@GetMapping("/actions")
-	public String firstPage() {
-		return "welcom";
-	}
-	
+	//------------------------------------------------------//
+    //           Chemin de lancement du PPT                 //
+    //------------------------------------------------------//
 	@GetMapping("/")
 	public String HomePage() {
 		GesteService PTTFile = new GesteService();
@@ -35,14 +35,24 @@ public class ApplicationController {
 		return "home";
 	}
 	
+	//------------------------------------------------------//
+    //           Chemin de reception du geste               //
+	//														//
+	// Parametre :											//
+	// 		Object GesteService : object contenant le nom 	//
+	//				du geste à effectué et diverses 		//
+	//				autres composants						//
+	//														//
+    //------------------------------------------------------//
 	@PostMapping("/getAPI")
 	public @ResponseBody ResponseEntity<List<Object>> getAPI(GesteService PTTFile) throws AWTException {
-		String new_geste = PTTFile.getGeste();
 		
-		PTTFile.DoGeste(new_geste);
+		String new_geste = PTTFile.getGeste();     	// Extraction du nom du geste dans une variable
+		
+		PTTFile.DoGeste(new_geste);					// Effectue le geste dans le module 
 		this.prev_geste = new_geste;
 		
-		return new ResponseEntity<List<Object>>(
+		return new ResponseEntity<List<Object>>(	// Affiche quelques composantes du l'Object receptionné sur une page web
 				Arrays.asList(
 				PTTFile.getGeste(),
 				PTTFile.getPosition()),
